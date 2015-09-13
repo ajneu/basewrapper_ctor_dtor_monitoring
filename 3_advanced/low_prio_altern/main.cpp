2c416b77e39577db262fc6ba6dc4fe4ef446d8a2
ajneu <ajneu@users.noreply.github.com>
2015-09-14 00:12:29 +0200

    code

--- 3_advanced/low_prio_altern/main.cpp
+++ 3_advanced/low_prio_altern/main.cpp
@@ -59,8 +59,73 @@ private:
 };
 
 
+template <typename TData>  // TData must have the from RefCountWithData<T>
+class RefCount;
 
-class RefCount {
+
+template <typename T>
+class RefCountWithData {
+  /*
+    Base class, that holds common data (such as a name!) with instances "of the same value".
+    The derived class needs to be RefCount.
+
+    Constructor takes a value of the data to hold, and a pointer (one to hold copy of data)
+    If a pointer is nullptr, then constructor allocates on heap; else pass in memory from outside [typically from stack].
+
+    The data is "tracked" and held among instances "of the same value" (achieved through the derived class RefCount, 
+    that implements the mechanism, and calls decrease())
+   */
+public:
+  using datatype = T;
+  RefCountWithData(const T &data_cpy, T *data_ = nullptr)
+    : data{data_}
+  {
+    *data = data_cpy;
+  }
+
+  RefCountWithData(const RefCountWithData& rhs)
+    : data{rhs.data}
+  {
+  }
+  
+  RefCountWithData &operator=(const RefCountWithData &rhs)
+  {
+    // d) increment
+    /* not used here */
+
+  }
+  
+  void decrease(const void *copy_assign_rhs, bool del)
+  {
+    // a) decrease
+    /* not used here */
+
+    if (del) {
+      // b) delete
+      data.delete1();
+    }
+
+    if (copy_assign_rhs) {
+      // c) copy_assign
+      data = static_cast<const RefCount<RefCountWithData<T>> *>(copy_assign_rhs)->data;
+
+      // d) increment
+      /* not used here */
+    }
+
+  }
+
+  const T &get_data()  const { return *data; }
+  
+private:
+  StackheapPtr<T> data;
+};
+
+
+
+
+template <typename TData>  // TData must have the from RefCountWithData<T>
+class RefCount : public TData {
   /*
     Reference Counting (constructor with nullptr, causes allocation on heap; else pass in memory from outside [typically stack]).
     This class tracks instances "of the same value"!   "same value" due to: copy construction, copy assignment.
@@ -76,20 +141,26 @@ class RefCount {
     .                                                         c) cnt_p = rhs.cnt_p
     .                                                         d) ++*cnt_p                 // additional instance "of the same value" as rhs
 
-    There also is a virtual function "hook" (virt_decrease), with which derived classes
-    are informed of the same changes, and can perform the same mechanism for their data
+    There also is a call to decrease(), to a function in the base-class, such that the base-class is
+    informed of the same changes, and can perform the same mechanism with its data.
+
+    Constructor takes a pointer (to hold the memory for the counter)
+    If a pointer is nullptr, then constructor allocates on heap; else pass in memory from outside [typically from stack].
+    Thereafter the constructor takes arguments, that are forwarded to the base-class.
+
    */
 public:
   using cnt_t = size_t;
 
-  RefCount(cnt_t *cnt = nullptr) :
-    cnt_p{cnt}
+  template <typename... Params>
+  RefCount(cnt_t *cnt = nullptr, Params&&... args) :
+    cnt_p{cnt}, TData{std::forward<Params>(args)...}
   {
     *cnt_p = 1U;
   }
   
   RefCount(const RefCount &rhs)
-    : cnt_p{rhs.cnt_p}
+    : TData{rhs}, cnt_p{rhs.cnt_p}
   {
     // d) increment
     ++*cnt_p;
@@ -114,21 +185,10 @@ public:
   
   virtual ~RefCount()
   {
-    /* Cannot call    decrease_cnt_check_del();    here,
-       since it calls virt_decrease, but for virtual functions in the destructor,
-       the derived class is already destructed, to the virtual function will not reach the derived class!
-
-       Solution: the derived class calls destructor_pre_cleanup_from_derived(), when it (the derived class is in the destructor)
-     */
-  }
-
-  void destructor_pre_cleanup_from_derived() // called from derived class's destructor (reason: the callstack will end up calling virt_decrease)
-  {
     // a) decrease    and if necessary   b) delete
     decrease_cnt_check_del();
   }
 
-  
   const cnt_t *get_shared_cnt_ptr()  const { return &*cnt_p;}
 
 private:
@@ -144,91 +204,33 @@ private:
       del = true;
       cnt_p.delete1();
     }
-    virt_decrease(copy_assign_rhs, del);
+    TData::decrease(copy_assign_rhs, del);
   }
 };
 
 
 
-template <typename T>
-class RefCountWithData : public RefCount {
-  /*
-    Wrapper derived class, that holds common data (such as a name!) with instances "of the same value"
 
-    Constructor takes a value of the data to hold, and then 2 pointers (one to hold copy of data, one to hold counter)
-    If a pointer is nullptr, then constructor allocates on heap; else pass in memory from outside [typically from stack].
-
-    The data is "tracked" and held among instances "of the same value" (achieved through the base class RefCount)
-   */
-public:
-  using datatype = T;
-  
-  RefCountWithData(const T &data_cpy, T *data_ = nullptr, RefCount::cnt_t *cnt = nullptr)
-    : RefCount{cnt}, data{data_}
-  {
-    *data = data_cpy;
-  }
-  
-  RefCountWithData &operator=(const RefCountWithData &rhs)
-  {
-    // d) increment
-    /* not used here */
-
-    RefCount::operator=(rhs);
-  }
-
-  ~RefCountWithData()
-  {
-    RefCount::destructor_pre_cleanup_from_derived();
-  }
-  
-  virtual void virt_decrease(const void *copy_assign_rhs, bool del)
-  {
-    // a) decrease
-    /* not used here */
-
-    if (del) {
-      // b) delete
-      data.delete1();
-    }
-
-    if (copy_assign_rhs) {
-      // c) copy_assign
-      data = static_cast<const RefCountWithData *>(copy_assign_rhs)->data;
-
-      // d) increment
-      /* not used here */
-    }
-
-  }
-
-  const T &get_data()  const { return *data; }
-  
-private:
-  StackheapPtr<T> data;
-};
-
-
-class BaseWrapper : public RefCountWithData<std::string> {
+class BaseWrapper : public RefCount<RefCountWithData<std::string>> {
   /* Class used for monitoring constructor and destructor behaviour.
      Also monitor instances "of the same value":      "same value" due to: copy construction, copy assignment.
 
      Other classes just need to derive from this class and get automatic monitoring.
 
-     Consructor (same as RefCountWithData, to which the arguments are forwarded):
+     Constructor (same as Base Class, to which the arguments are forwarded):
      Constructor takes a value of the name to hold, and then 2 pointers (one to hold copy of name, one to hold counter)
      If a pointer is nullptr, then constructor allocates on heap; else pass in memory from outside [typically from stack].
   */
 public:
-  BaseWrapper(const std::string &name_, std::string *name_ptr = nullptr, RefCount::cnt_t *cnt_ptr = new RefCount::cnt_t)
-    : RefCountWithData<std::string>{name_, name_ptr, cnt_ptr}
+  BaseWrapper(const std::string &name_, std::string *name_ptr = nullptr, RefCount::cnt_t *cnt_ptr = nullptr)
+    : RefCount<RefCountWithData<std::string>>{cnt_ptr, name_, name_ptr}
   {
     std::cerr << "#constructor      ";
     print_info(std::cerr) << std::endl;
   }
   
   BaseWrapper(const BaseWrapper &rhs)
-    : RefCountWithData<std::string>{rhs}
+    : RefCount<RefCountWithData<std::string>>{rhs}
   {
     std::cerr << "#copy-constructor ";
     print_info(std::cerr) << std::endl;
@@ -243,7 +245,8 @@ public:
         return *this;
     }
 
-    RefCountWithData<std::string>::operator=(rhs);
+    //RefCountWithData<std::string>::operator=(rhs);
+    RefCount<RefCountWithData<std::string>>::operator=(rhs);
     std::cerr << "\t ==>  ";
     print_info(std::cerr) << std::endl;
     return *this;
@@ -255,19 +258,21 @@ public:
     print_info(std::cerr) << std::endl;
   }
 
-private:
+ private:
   std::ostream& print_info(std::ostream &os) {
     os << "cnt_p " << get_shared_cnt_ptr() << " \tthis " << this << " \t" << get_data() << " (" << *get_shared_cnt_ptr() << ')';
   }
 };
 
 
-
 int main()
 {
+  RefCount<RefCountWithData<std::string>> x{nullptr, "asdf", nullptr};
+
+
   BaseWrapper::datatype /*std::string*/ name;
   BaseWrapper::cnt_t    /*size_t     */ cnt;
-
+  
   BaseWrapper br1{"br1", &name, &cnt};
   BaseWrapper br2{br1};
   BaseWrapper br3{"br3"};
